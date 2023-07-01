842c882c23d0a4d4b1b67200a09380c1e6d3cebf
Nishant <shokeennishant444@gmail.com>
2023-07-02 01:31:37 +0530

    02-07-2023

--- App.js
+++ App.js
@@ -7,9 +7,10 @@ function fetchAPIExample() {
         .then((response) => {
             let tempId = 0;
             let result = ""
+            // response.results.sort((a, b) => a.name - b.name)
             response.results.map(data => {
-                const imgeUrl = image_base_path + data.poster_path;
-                result += `<div class="main-container" >
+                    const imgeUrl = image_base_path + data.poster_path;
+                    result += `<div class="main-container" >
              <div class="poster-container" >
              <button class="m-button"  type="button"  data-bs-toggle="modal" data-bs-target="#${data.id}" href="#" ><img  src="${imgeUrl}" class="poster"/></button>
              <div class="modal fade" id="${data.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -20,18 +21,26 @@ function fetchAPIExample() {
                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                      </div>
                      <div class="modal-body">
+                  
                      <img src = "${imgeUrl}" />
-                  <div class = "title">${data.original_name} </br>
-                  <span class = "rating-board"> 8.2/10</span> </br>
-                  <span class = "lang"> ${data.original_language.toUpperCase()} </span>
-                
-                  </div> 
+                  <div class = "title.main"><span class="main-title">${data.original_name}</span> 
+                 
+                  </br>
+                  <div class = "center-text"> 
+                  <span class = "lang">  ${data.original_language.toUpperCase()} </span> 
+                 
+                  <span class = "rating-board.main">⭐️${data.vote_average}</span> 
+                  <span class = "rating-board.main">₹250</span>
+                 </div>
+                 
+                  <div class = "desc">${data.overview}</div>
+                  <a href = "checkout-page.html"> <button class="glow-on-hover data-bs-dismiss="modal" onclick= "storeName('` + data.original_name+ `')">Book Now ! </a></button>
+                    </div> 
                   </div>
                  <div class = "title"> </div>
-                     <div class="modal-footer">
-                        <a href = "checkout-page.html"> <button  type="button" class="btn-second" data-bs-dismiss="modal" onclick= "storeName('` + data.original_name+ `')">Book Now</button> </a>
-                       
-                     </div>
+                 
+                  
+                 
                  </div>
              </div>
          </div>
@@ -41,12 +50,15 @@ function fetchAPIExample() {
                   <h5 class="movie-title">
                      ${data.original_name}
                   </h5>
-                  <h6 class="movie-info">Lang : ${data.original_language.toUpperCase()} <span class="rating">8.2</span></h6>
+                  
+                  <h6 class="movie-info">Lang : ${data.original_language.toUpperCase()} <span class="rating">${data.vote_average}</span></h6>
+                
                </div>
             </div>
          </div>`
-            }).join()
+                }).join()
             document.getElementById("hero-container").innerHTML = result;
+
         });
 
 }
@@ -62,16 +74,14 @@ function genreList() {
      .then(res => res.json())
      .then(res => {
         for (const genre of res.genres) {
-          genreList += `<button class = "genre-btn"><li class="menu-item menu-item-type-post_type" id="${genre.id}">${genre.name}</li></button>`;
+          genreList += `<button class = "genre-btn" onclick="searchMovie('${genre.name}')"><li class="menu-item menu-item-type-post_type" id="${genre.id}">
+          ${genre.name}</button></li>`;
         }
         document.getElementById("genre-list").innerHTML = genreList;
       })
      .catch(err => console.log(err));
   }
   genreList();
-//input--empty (param)
-//it snot inputfiled
-
 
 //  -------------------- For Search Functionality -------------------
 
@@ -79,25 +89,26 @@ function searchMovie(movieName) {
     // event.preventDefault(); -- form sends data to server and reloads the page but data is not going anywhere
     const MOVIE_BASE = "https://api.themoviedb.org/3/search/movie?api_key=f7deed720cf5347ef72ca2f166de2c19&language=en-US&query=";
     const MOVIE_URL_SUFFIX = "&page=1&include adult=false";
-    const searchTerm = document.getElementById("search-field").value;
-    for (let i = 0; i < searchTerm; i++) {
-        if (searchTerm == "") {
-         console.log(searchMovie(movieName));
-             } else {
-            console.log("search-field");
-        }
+    let searchTerm = document.getElementById("search-field").value;
+    if (movieName != undefined) {
+        searchTerm = movieName
     }
-   
-    const movieUrl = MOVIE_BASE + searchTerm + MOVIE_URL_SUFFIX;
+const movieUrl = MOVIE_BASE + searchTerm + MOVIE_URL_SUFFIX;
     fetch(movieUrl)
         .then((response) => response.json())
         .then((response) => {
             let tempId = 0;
             let result = ""
             response.results.map(data => {
-                const imgeUrl = image_base_path + data.poster_path;
+                console.log(data.original_title);
+                let imgeUrl = ""
+                if (data.poster_path == null || data.poster_path == undefined) {
+                    imgeUrl = "avatar-wow.jpg"
+                } else {
+                    imgeUrl = image_base_path + data.poster_path
+                }
                 result += `<div class="main-container" >
-             <div class="poster-container" >
+           <div class="poster-container" >
              <button class="m-button"  type="button"  data-bs-toggle="modal" data-bs-target="#${data.id}" href="#" ><img  src="${imgeUrl}" class="poster"/></button>
              <div class="modal fade" id="${data.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
              <div class="modal-dialog modal-dialog-centered">
@@ -107,18 +118,24 @@ function searchMovie(movieName) {
                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                      </div>
                      <div class="modal-body">
+                  
                      <img src = "${imgeUrl}" />
-                  <div class = "title">${data.original_name} </br>
-                  <span class = "rating-board"> 8.2/10</span> </br>
-                  <span class = "lang"> ${data.original_language.toUpperCase()} </span>
-                
+                  <div class = "title.main"><span class="main-title">${data.original_title}</span> 
+                 
+                  </br>
+                  <div class = "center-text"> 
+                  <span class = "lang">  ${data.original_language.toUpperCase()} </span> 
+                 
+                  <span class = "rating-board.main">⭐️${data.vote_average}</span> 
+                  <span class = "rating-board.main">₹250</span>
+                 </div>
+                 
+                  <div class = "desc">${data.overview}</div>
+                 <a href = "checkout-page.html"> <button class="glow-on-hover" type="button">Book Now !</button></a>
                   </div> 
                   </div>
                  <div class = "title"> </div>
-                 <div class="modal-footer">
-                 <a href = "checkout-page.html"> <button  type="button" class="btn-second" data-bs-dismiss="modal" onclick= "storeName('` + data.original_name+ `')">Book Now</button> </a>
-                
-              </div>
+                 
                  </div>
              </div>
          </div>
@@ -126,9 +143,11 @@ function searchMovie(movieName) {
             <div class="box-container">
                <div class="ticket__content">
                   <h5 class="movie-title">
-                     ${data.original_name}
+                     ${data.original_title}
                   </h5>
-                  <h6 class="movie-info">Lang : ${data.original_language.toUpperCase()} <span class="rating">8.2</span></h6>
+                  
+                  <h6 class="movie-info">Lang : ${data.original_language.toUpperCase()} <span class="rating">${data.vote_average}</span></h6>
+                
                </div>
             </div>
          </div>`
@@ -137,32 +156,32 @@ function searchMovie(movieName) {
         });
 
 }
-searchMovie(movieName);
+
 
 
 
 
 //  -------------------- For Movie Name -------------------
 
-function storeName(name){
-    localStorage.setItem("moviename",name);
-    
+function storeName(name) {
+    localStorage.setItem("moviename", name);
+
 }
 
 
- // --------------------For Credit Card-------------------
+// --------------------For Credit Card-------------------
 
 function displayPayment() {
-   
+
     var divc = document.getElementById("payment-area-credit");
     let radioChoice = document.querySelectorAll('input[name="payment"]:checked')
-    radioChoice.checked=false;
-    divc.style.display = radioChoice[0].value ==  "C" ? "inline" : "none";
+    radioChoice.checked = false;
+    divc.style.display = radioChoice[0].value == "C" ? "inline" : "none";
     var diva = document.getElementById("payment-area-upi");
     diva.style.display = "none";
     var divb = document.getElementById("payment-area-debit");
     divb.style.display = "none";
-   
+
 }
 
 
@@ -172,50 +191,51 @@ function displayPayment() {
 function displayPay() {
     let radioChoice = document.querySelectorAll('input[name="payment"]:checked')
     var divb = document.getElementById("payment-area-debit");
-    divb.style.display = radioChoice[0].value ==  "B" ? "inline" : "none";
+    divb.style.display = radioChoice[0].value == "B" ? "inline" : "none";
     var divc = document.getElementById("payment-area-credit");
-    divc.style.display = "none" ;
+    divc.style.display = "none";
     var diva = document.getElementById("payment-area-upi");
     diva.style.display = "none";
-    }
+}
 
 // // --------------------For UPI-------------------
 
 function displayUpi() {
     let radioChoice = document.querySelectorAll('input[name="payment"]:checked')
-   var diva = document.getElementById("payment-area-upi");
-    diva.style.display = radioChoice[0].value == "A" ? "inline" :  "none"  ;
+    var diva = document.getElementById("payment-area-upi");
+    diva.style.display = radioChoice[0].value == "A" ? "inline" : "none";
     var divb = document.getElementById("payment-area-debit");
     divb.style.display = "none";
-   }
+}
 
 
-   function showTotal(){
-    var storage = document.getElementById("notickets") 
+function showTotal() {
+    var storage = document.getElementById("notickets")
     var tax = document.getElementById("fees")
-    var notickets =  parseInt(storage.value)
-    var convenienceFee =  1.75*(250 / 100)*notickets
+    var notickets = parseInt(storage.value)
+    var convenienceFee = 1.75 * (250 / 100) * notickets
     var fees = document.getElementById("fees")
     fees.innerHTML = convenienceFee;
-   }
-   showTotal(); 
+}
+//    showTotal(); 
 
-   function setTotal() {
-    
+function setTotal() {
+
     var price = document.getElementById("notickets");
-    var notickets =  parseInt(price.value);
-    var convenienceFee =  1.75*(250 / 100)*notickets;
-    var total = convenienceFee + (notickets*250) ;
-    
-    console.log(total);
-    document.getElementById("finalFees").innerHTML=total;
-    }
-    setTotal();
-   
+    var notickets = parseInt(price.value);
+    var convenienceFee = 1.75 * (250 / 100) * notickets;
+    var total = convenienceFee + (notickets * 250);
 
+    console.log(total);
+    document.getElementById("finalFees").innerHTML = total;
+}
+setTotal();
+
+function storeName(name){
+    localStorage.setItem("moviename", name);
+}
 
     // function paySubmit(){
     //     alert("THANK YOU. Your Seats are Booked and Ticket are sent to your Email address");
     // }
     // paySubmit();
-    
